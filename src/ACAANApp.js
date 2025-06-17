a846a74531f2e46ca3ba49af00c7c74a4871aeac
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-06-17 10:56:22 +0300

    Update ACAANApp.js

--- src/ACAANApp.js
+++ src/ACAANApp.js
@@ -94,9 +94,9 @@ const normalizeCardInput = (value) => {
 export default function ACAANApp() {
   //const [selectedStackName, setSelectedStackName] = useState(null);
   const [selectedStackName, setSelectedStackName] = useState("Mnemonica");
-  //const [deck, setDeck] = useState(stacks["Mnemonica"]);
+  const [deck, setDeck] = useState(stacks["Mnemonica"]);
   // yllä oletukseksi määritelty ilman valikkoa Mnemonica
-  const [deck, setDeck] = useState([]);
+  //const [deck, setDeck] = useState([]);
   const [cardPosition, setCardPosition] = useState(null);
   const [numberRange, setNumberRange] = useState(null);
   const [instructions, setInstructions] = useState({});
